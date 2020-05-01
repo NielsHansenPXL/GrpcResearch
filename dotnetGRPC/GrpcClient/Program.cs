1c343dc91241b9f243b9121ea06b0efa514bb72f
wvince <vince.wouters@gmail.com>
2020-05-01 23:48:07 +0200

    Changed ports, added readline and setswitch

--- dotnetGRPC/GrpcClient/Program.cs
+++ dotnetGRPC/GrpcClient/Program.cs
@@ -10,11 +10,13 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            var channel = GrpcChannel.ForAddress("https://localhost:8081");
             var artistClient = new RemoteArtist.RemoteArtistClient(channel);
             var artistInput = new ArtistLookUpModel() { ArtistId = 22 };
             var artistReply = await artistClient.GetArtistInfoAsync(artistInput);
             Console.WriteLine($"{artistReply.Name}");
+            Console.ReadLine();
         }
     }
 }
